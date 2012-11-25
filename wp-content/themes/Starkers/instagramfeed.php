c1c7587b8e4268054dc5f8813adce3aa75b5d3c3
jllord <jessica@codeforamerica.org>
2012-11-25 11:48:43 -0800

    instagram page to show only the last 16

--- wp-content/themes/Starkers/instagramfeed.php
+++ wp-content/themes/Starkers/instagramfeed.php
@@ -17,7 +17,7 @@
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 <div id="content" class="instagramFeed">
 	<div id="instagram" class="span4"><span class="spinner"><img src="http://jlord.s3.amazonaws.com/wp-content/uploads/fbi_spinner1.gif"></span></div><div class="clear"></div>
-	<div class="span3"><p>These are my Instagram photos starting when I started saving them to a Google Spreadsheet using <a href="http://www.ifttt.com" target="_blank">ifttt.com</a>. My <a href="http://jlord.us/your-own-instagram-feed/">blog post</a> on how.</p></div>
+	<div class="span3"><p>These are the last 16 pictures in my Instagram feed. Here is my <a href="http://jlord.us/your-own-instagram-feed/">blog post</a> on how.</p></div>
 </div> 
 
 <script src="/wp-content/themes/Starkers/sheetsee.js?0"></script>
@@ -37,7 +37,7 @@
 
   showDataA = function(data) {
 		var instagram = ich.instagram({
-			"rows": data.reverse()
+			"rows": getLast(data,26)
 		})
 		document.getElementById('instagram').innerHTML = instagram;
 	}
