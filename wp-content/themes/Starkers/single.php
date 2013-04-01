fb18efa99b7d3218f53a2e65bb5d452751a5f060
jllord <jessica@codeforamerica.org>
2013-03-31 22:29:17 -0700

    removed comments

--- wp-content/themes/Starkers/single.php
+++ wp-content/themes/Starkers/single.php
@@ -46,15 +46,7 @@
 
 
 </div>
-<div class="comments">
-	<?php if ( get_the_author_meta( 'description' ) ) : ?>
-	<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-	<h3>About <?php echo get_the_author() ; ?></h3>
-	<?php the_author_meta( 'description' ); ?>
-	<?php endif; ?>
 
-	<?php comments_template( '', true ); ?>
-</div>
 </article>
 <div class="span1 noMobile">
 	<div id="blogSidebar">
