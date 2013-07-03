b4879b7043254b4a23b0fbd4f16f256684f37d39
jllord <jessica@codeforamerica.org>
2013-07-02 19:16:51 -0700

    added styles to category page

--- wp-content/themes/Starkers/category.php
+++ wp-content/themes/Starkers/category.php
@@ -10,15 +10,18 @@
  */
 ?>
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-
+<div id="content" class="archivePage">
 <?php if ( have_posts() ): ?>
+	<div class="span1">
 <h2>Category Archive: <?php echo single_cat_title( '', false ); ?></h2>
+	</div>
+	<div class="span3">
 <ol>
 <?php while ( have_posts() ) : the_post(); ?>
 	<li>
 		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+			<h1 class="postTitle archive"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+			<h6 class="postDate"><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?></h6>
 			<?php the_content(); ?>
 		</article>
 	</li>
@@ -27,5 +30,7 @@
 <?php else: ?>
 <h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
 <?php endif; ?>
+</div>
+</div>
 
 <?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
