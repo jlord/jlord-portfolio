b4b761675be5a1e7b2126fc1a3a669958d512db0
jllord <jessica@codeforamerica.org>
2013-07-17 21:39:43 -0700

    added italic to merriweather

--- wp-content/themes/Starkers/parts/shared/html-header.php
+++ wp-content/themes/Starkers/parts/shared/html-header.php
@@ -16,7 +16,7 @@
 		
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png"/>
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600|Merriweather:400,700' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600|Merriweather:400,700,400italic' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
 		<script type="text/javascript">
 		  var _gaq = _gaq || [];
